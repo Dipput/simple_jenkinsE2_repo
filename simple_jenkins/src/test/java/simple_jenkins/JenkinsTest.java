f04b9bca6df86b35a4893bff5a3eb3e7c2aa5fa0
vipen <vipen@Hi-Speed-S340>
2025-02-06 13:54:55 +0530

    Pollscm commit

--- simple_jenkins/src/test/java/simple_jenkins/JenkinsTest.java
+++ simple_jenkins/src/test/java/simple_jenkins/JenkinsTest.java
@@ -1,5 +1,9 @@
 package simple_jenkins;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 @Test
@@ -9,6 +13,12 @@ public class JenkinsTest {
 	
 	public void jenkinsTest()
 	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://demowebshop.tricentis.com");
 		Reporter.log("Jenkins Test Class executed",true);
+		Reporter.log("Demo class got executed",true);
 	}
 }
---
+++ simple_jenkins/src/test/java/simple_jenkins/PullscmTest.java
@@ -0,0 +1,13 @@
+package simple_jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PullscmTest {
+
+	@Test
+	public void test()
+	{
+		Reporter.log("pollscn class executed",true);
+	}
+}
